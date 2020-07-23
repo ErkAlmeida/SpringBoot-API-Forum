3073bd48d81b99521dd29d01ab9746821d60af91
Erick Almeida <erickprofissional@gmail.com>
2020-07-23 00:38:54 -0300

    Configuração das dependencias JPA e H2, Criado arquivo data.sql para inserir dados no banco

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO USUARIO(nome,email,senha) VALUES('Erick','erickprofissional@gmail.com','123456');
+
+
+INSERT INTO CURSO(nome,categoria) VALUES('Spring Boot','Programação');
+INSERT INTO CURSO(nome,categoria) VALUES('JavaScript','Front-end');
+
+INSERT INTO TOPICO(titulo, mensagem,data_Criacao, status, autor_id, curso_id) VALUES('Duvida 0','Como se programa','2020-07-23 00:16:00','NAO_RESPONDIDO',1,1);
+INSERT INTO TOPICO(titulo, mensagem,data_Criacao, status, autor_id, curso_id) VALUES('Duvida 1','Configurar o Maven','2020-07-23 00:26:00','NAO_RESPONDIDO',1,1);
+INSERT INTO TOPICO(titulo, mensagem,data_Criacao, status, autor_id, curso_id) VALUES('Duvida 0','Como configurar o React','2020-07-24 01:10:00','NAO_RESPONDIDO',1,2);
+
